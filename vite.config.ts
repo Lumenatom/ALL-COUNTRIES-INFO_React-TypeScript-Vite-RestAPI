82a1ba2957fe19ffce56ccec4176636e9df8b588
Lumenatom <lumenatom@gmail.com>
2023-03-22 20:44:17 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/<ALL-COUNTRIES-INFO_React-TypeScript-Vite-RestAPI>/',
+});
