e0b71e99e8a899e331a1155010065af480151278
Lumenatom <lumenatom@gmail.com>
2023-03-25 16:33:54 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",
+  base: "/All_Countries_Info/",
   plugins: [react()],
 });
